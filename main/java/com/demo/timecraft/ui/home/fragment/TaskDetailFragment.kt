8d5acdb7ae056e8b95b352e39ee8626ca740784a
harsh <harshjasani12345@gmail.com>
2020-04-20 15:27:00 +0530

    timeCraft in Demo Branch

--- main/java/com/demo/timecraft/ui/home/fragment/TaskDetailFragment.kt
+++ main/java/com/demo/timecraft/ui/home/fragment/TaskDetailFragment.kt
@@ -13,16 +13,12 @@ import com.demo.timecraft.ui.home.adapter.RecycleViewTodayTaskAdapter
 import com.demo.timecraft.ui.home.adapter.callBackBottomSheet
 import kotlinx.android.synthetic.main.home_fragment_task_detail.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class TaskDetailFragment : Fragment(), callBackBottomSheet {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.home_fragment_task_detail, container, false)
     }
 
